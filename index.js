38a1764236b253d7163f4947d443e47cc627453a
sourav-18 <souravhd2@gmail.com>
2026-02-06 12:43:28 +0530

    db

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require("fix-esm").register()
 require("dotenv").config();
-require("./db/mongo/conn.mongo")()
 
 const express = require("express")
 const cors = require("cors");
@@ -10,8 +9,8 @@ const envUtil = require("./utils/env.util");
 const dateUtil = require("./utils/date.util");
 const globalErrorHandlerMiddleware = require("./middlewares/globalErrorHandler.middleware");
 const authenticationMiddleware = require("./middlewares/authentication.middleware");
+const connectDB = require("./db/mongo/conn.mongo");
 
-require("./cronJob/task.cronJob");
 
 //external middleware use
 app.use(cors(
@@ -23,35 +22,43 @@ app.use(cors(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req,res,next)=>{
-    if(!req.body){
-        req.body={}
+app.use((req, res, next) => {
+    if (!req.body) {
+        req.body = {}
     }
     next();
 })
 
 // main router initialize
-app.use("/api/v1", authenticationMiddleware.checkAppId, require("./routes/main.route"));
 
-app.use(globalErrorHandlerMiddleware);
+async function startServer() {
 
-app.use((req,res)=>{
-    res.send("hello")
-})
+    await connectDB()
+    
+    require("./cronJob/task.cronJob");
 
+    app.use("/api/v1", authenticationMiddleware.checkAppId, require("./routes/main.route"));
 
+    app.use(globalErrorHandlerMiddleware);
 
-app.listen(envUtil.SERVER_PORT, (err) => {
-    if (!err) {
-        const serverStartInfo = {
-            message: "Server started",
-            port: envUtil.SERVER_PORT,
-            host: envUtil.SERVER_ENVIRONMENT,
-            time: dateUtil.getByFormat()
+    app.use((req, res) => {
+        res.send("hello")
+    })
+
+    app.listen(envUtil.SERVER_PORT, (err) => {
+        if (!err) {
+            const serverStartInfo = {
+                message: "Server started",
+                port: envUtil.SERVER_PORT,
+                host: envUtil.SERVER_ENVIRONMENT,
+                time: dateUtil.getByFormat()
+            }
+            console.log(serverStartInfo);
         }
-        console.log(serverStartInfo);
-    }
-});
+    });
+}
+
+startServer();
 
 
-module.exports = app;
+
